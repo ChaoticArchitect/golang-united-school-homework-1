452c9c61b2121288df758fab19d83df1c6a27634
Anri_Kezeroti <anri.kezeroti@wearewoven.com>
2022-02-11 21:45:52 +0400

    done

---
+++ helloWorld.go
@@ -0,0 +1,11 @@
+package golang_united_school_homework_1
+
+import (
+	"fmt"
+	"github.com/kyokomi/emoji/v2"
+)
+
+func main() {
+	pizzaMessage := emoji.Sprint("Hello 🗺️!")
+	fmt.Println(pizzaMessage)
+}
